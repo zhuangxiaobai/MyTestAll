e7c4a565c48e7b9f4e6821d6fa8e777c4b781fe8
庄小白 <584787434@qq.com>
2020-12-09 17:13:02 +0800

    mybatisplus crud例子

--- mybatis_plus_demo/src/test/java/com/zc/mybatis_plus_demo/MybatisPlusDemoApplicationTests.java
+++ mybatis_plus_demo/src/test/java/com/zc/mybatis_plus_demo/MybatisPlusDemoApplicationTests.java
@@ -1,7 +1,10 @@
 package com.zc.mybatis_plus_demo;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.druid.support.json.JSONUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zc.mybatis_plus_demo.entity.ELog;
@@ -16,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MybatisPlusDemoApplicationTests {
@@ -41,6 +46,7 @@ class MybatisPlusDemoApplicationTests {
         evote.setType(4);
         eVoteMapper.insert(evote);*/
         //-------------------
+
         //删除-----------------如果设置了逻辑删除此处会变为逻辑删除
         //根据 entity 条件，删除记录
         /*QueryWrapper<EVote> wrapper = new QueryWrapper<>();
@@ -51,51 +57,95 @@ class MybatisPlusDemoApplicationTests {
         idList.add(1);
         idList.add(2);
         eVoteMapper.deleteBatchIds(idList);*/
-//        // 根据 ID 删除
+        // 根据 ID 删除
        // eVoteMapper.deleteById(3);
-//        // 根据 columnMap 条件，删除记录
+        // 根据 columnMap 条件，删除记录
    /*    HashMap<String,Object> map = new HashMap<>();
         map.put("name","asd");
         eVoteMapper.deleteByMap(map);*/
        //---------------------------
 
-        //
-//        EVote evote = new EVote();
-//        evote.setId(2);
-//        QueryWrapper queryWrapper = new QueryWrapper<>();
-//        queryWrapper.eq("name","bb");
-//        queryWrapper.setEntity(evote);
-//        eVoteMapper.selectList(queryWrapper);
-//
 
+        //修改----------------------
+        // 根据 whereEntity 条件，更新记录
+        /*UpdateWrapper<EVote> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("name","asd");
+        EVote eVote = new EVote();
+        eVote.setType(10);
+        eVoteMapper.update(eVote,updateWrapper);//前面是set的值，后面是条件 */
+        // 根据 ID 修改
+        /*EVote eVote = new EVote();
+        eVote.setId(1);
+        eVote.setType(12);
+        eVoteMapper.updateById(eVote);
+        */
+        //---------------------------
 
+        //查询------------------------
+        // 根据 ID 查询
+        //EVote eVote = eVoteMapper.selectById(1);
+        // 根据 entity 条件，查询一条记录
+          /*QueryWrapper<EVote>  queryWrapper = new QueryWrapper<>();
+          queryWrapper.eq("id",1);
+          eVoteMapper.selectOne(queryWrapper);
+          */
+        // 查询（根据ID 批量查询）
+       /* ArrayList<Integer> idList = new ArrayList();
+        idList.add(1);
+        idList.add(2);
+        List<EVote> eVotes  = eVoteMapper.selectBatchIds(idList);
+      */
+        // 根据 entity 条件，查询全部记录
+        /*QueryWrapper<EVote>  queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name","asd");
+        List<EVote> votes  = eVoteMapper.selectList(queryWrapper);*/
+        // 查询（根据 columnMap 条件）
+        /*HashMap<String,Object> map = new HashMap<>();
+        map.put("name","asd");
+        eVoteMapper.selectByMap(map);*/
+        // 根据 Wrapper 条件，查询全部记录
+        /*QueryWrapper<EVote>  queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name","asd");
+        List<Map<String, Object>> mapList =  eVoteMapper.selectMaps(queryWrapper);
+        */
+        // 根据 Wrapper 条件，查询全部记录。注意： 只返回第一个字段的值
+        /*QueryWrapper<EVote>  queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name","asd");
+        List<Object>  objects  = eVoteMapper.selectObjs(queryWrapper);
+         */
 
+        // 根据 entity 条件，查询全部记录（并翻页）(entity 和 wrapper条件叠加)
+        /*Page<EVote> page = new Page();
+        page.setSize(2);
+        page.setCurrent(1);
+//        page.setSearchCount(false);
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("name","asd");
+//        EVote eVote = new EVote();
+//        eVote.setName("aaa");
+//        queryWrapper.setEntity(eVote);
+        Page<EVote> eVotePage = eVoteMapper.selectPage(page, queryWrapper);
+        System.out.println(JSONUtil.parse(eVotePage).toString());
+       */
 
-     // QueryWrapper queryWrapper = new QueryWrapper();
-        // eLogMapper.selectList();
-    //      ELog eLog = eLogMapper.selectById(1);
-    //      System.out.println(eLog.toString());
+        // 根据 Wrapper 条件，查询全部记录（并翻页）//默认是一页10条 页数第1页
+        /*Page<Map<String, Object>> page = new Page<>();
+//        page.setSize(2);
+//        page.setCurrent(1);
+//        page.setSearchCount(false);
+        QueryWrapper<EVote> queryWrapper = new QueryWrapper();
+ //       queryWrapper.eq("name","asd");
+//        EVote eVote = new EVote();
+//        eVote.setName("aaa");
+//        queryWrapper.setEntity(eVote);
+        Page<Map<String, Object>> mapPage  = eVoteMapper.selectMapsPage(page,queryWrapper);
+        System.out.println(JSONUtil.parse(mapPage).toString());
+        */
 
-//
-//            Page<ELog> page = new Page();
-//            page.setSize(2);
-//            page.setCurrent(1);
-//           // page.setSearchCount(false);
-//            QueryWrapper queryWrapper = new QueryWrapper(null);
-//            Page<ELog> page1 = eLogMapper.selectPage(page, queryWrapper);
-//            System.out.println(page1);
-
-
-
-
-//
-//
-//            EVote evote = new EVote();
-//            evote.setId(2);
-//           UpdateWrapper<EVote> updateWrapper = new UpdateWrapper<>();
-//           updateWrapper.set("deleted",1);
-//
-//           eVoteMapper.update(evote,updateWrapper);
+        // 根据 Wrapper 条件，查询总记录数
+        /*QueryWrapper<EVote> queryWrapper = new QueryWrapper();
+        queryWrapper.eq("name","asd");
+        Integer  size = eVoteMapper.selectCount(queryWrapper);*/
 
 
 
